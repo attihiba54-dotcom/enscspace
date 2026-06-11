c3ef7bf51c86a1dbb8cb1942e8b2e5e7f4d43b14
attihiba54-dotcom <attihiba54@gmail.com>
2026-06-11 18:41:51 +0200

    updat

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
-self.addEventListener('fetch', (event) => {});
+const CACHE_NAME = 'ensc-space-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/idex.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
